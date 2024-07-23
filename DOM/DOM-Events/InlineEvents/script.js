96958c3e7ece5b56f093a11cbfb3686a6e4ecc9d
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-07-23 12:03:17 +0530

    Inline Events practiced

---
+++ DOM/DOM-Events/InlineEvents/script.js
@@ -0,0 +1,38 @@
+//-- Mouse Event --
+
+function clickHandler() {
+  console.log("Click event is called");
+}
+function dbClickHandler() {
+  console.log("Double Click event is called");
+}
+function dbClickHandler() {
+  console.log("Double Click event is called");
+}
+function onMouseOverHandler() {
+  console.log("onMouseOverHandler Event is called");
+}
+function onMouseOutHandler() {
+  console.log("onMouseOutHandler event is called");
+}
+
+//--- Keyboard Event ---
+function onKeyDownHandler() {
+    console.log("onKeyDownHandler event is called");
+}
+function onKeyUpHandler() {
+    console.log("onKeyUpHandler event is called");
+}
+function onKeyPressHandler() {
+    console.log("onKeyPressHandler event is called");
+}
+
+//--- Form Events ---
+
+function onsubmitHandler() {
+  console.log("onsubmitHandler event is called");
+}
+function onfocusHandler() {
+  console.log("onfocusHandler event is called");
+}
+
